980bc594c7dfbafcc6aa7730f932e02534c4b097
zales <o.zalesky@gmail.com>
2024-07-05 01:25:09 +0200

    WIP move static content

--- zlsnasdisplay/display_renderer.py
+++ zlsnasdisplay/display_renderer.py
@@ -45,6 +45,59 @@ class DisplayRenderer:
         # Draw a horizontal line
         self.draw.line([(0, 110), (297, 110)], fill=0, width=0)
 
+        ## CPU
+        self.draw.rectangle((0, 0, 97, 67), fill=255)
+        self.draw.line(((26, 10), (97, 10)), fill=0, width=0)
+        self.draw.text((1, 0), "cpu", font=self.font14, fill=0)
+        # Draw CPU usage
+        self.draw.text((10, 12), "\ue30d", font=self.nfont24, fill=0)  # Unicode icon for CPU
+        self.draw.text(
+            (10, 42), "\ue1ff", font=self.nfont24, fill=0
+        )  # Unicode icon for temperature
+
+        ## UPDATES
+        self.draw.text((204, 67), "apt", font=self.font14, fill=0)
+        self.draw.line([(226, 76), (248, 76)], fill=0, width=0)
+
+        ## CHECK NET
+        self.draw.text((250, 67), "net", font=self.font14, fill=0)
+        self.draw.line([(249, 76), (249, 110)], fill=0, width=0)
+        self.draw.line([(272, 76), (297, 76)], fill=0, width=0)
+
+        # MEM
+        self.draw.text((1, 67), "mem", font=self.font14, fill=0)
+        self.draw.line([(34, 76), (97, 76)], fill=0, width=0)
+        self.draw.text((10, 80), "\ue322", font=self.nfont24, fill=0)  # Unicode icon for memory
+
+        # DISK
+        self.draw.text((100, 0), "nvme", font=self.font14, fill=0)
+        self.draw.line([(138, 10), (203, 10)], fill=0, width=0)
+        self.draw.text(
+            (108, 12), "\uf7a4", font=self.nfont24, fill=0
+        )  # Unicode icon for Hard Drive
+        self.draw.text(
+            (108, 42), "\ue1ff", font=self.nfont24, fill=0
+        )  # Unicode icon for temperature
+
+        # FAN
+        self.draw.text((100, 67), "fan", font=self.font14, fill=0)
+        self.draw.line([(122, 76), (203, 76)], fill=0, width=0)
+        self.draw.text((108, 80), "\uf168", font=self.nfont24, fill=0)  # Unicode icon for fan
+
+        # IP
+        self.draw.text((5, 110), "\ue80d", font=self.nfont14, fill=0)  # Unicode icon for network
+
+        # UPTIME
+        self.draw.text((205, 110), "\ue923", font=self.nfont14, fill=0)  # Unicode icon for uptime
+
+        # TRAFFIC
+        self.draw.text((204, 0), "down", font=self.font14, fill=0)
+        self.draw.line([(242, 10), (261, 10)], fill=0, width=0)
+        self.draw.text((208, 10), "\uf090", font=self.nfont24, fill=0)  # Unicode icon download
+        self.draw.text((204, 33), "up", font=self.font14, fill=0)
+        self.draw.line([(222, 43), (261, 43)], fill=0, width=0)
+        self.draw.text((208, 44), "\uf09b", font=self.nfont24, fill=0)  # Unicode icon for upload
+
     def update_display_and_save_image(self):
         """Update the display and save the image"""
         self.display_controller.update_display(self.image)
@@ -53,19 +106,11 @@ class DisplayRenderer:
 
     def render_cpu_load(self):
         """Render CPU load"""
-        self.draw.rectangle((0, 0, 97, 67), fill=255)
 
-        self.draw.line(((26, 10), (97, 10)), fill=0, width=0)
-
-        self.draw.text((1, 0), "cpu", font=self.font14, fill=0)
-        # Draw CPU usage
-        self.draw.text((10, 12), "\ue30d", font=self.nfont24, fill=0)  # Unicode icon for CPU
         self.draw.text(
             (40, 12), f"{SystemOperations().get_cpu_load()}%", font=self.font24, fill=0
         )  # CPU percentage
-        self.draw.text(
-            (10, 42), "\ue1ff", font=self.nfont24, fill=0
-        )  # Unicode icon for temperature
+
         self.draw.text(
             (40, 42), f"{SystemOperations().get_cpu_temperature()}°C", font=self.font24, fill=0
         )  # CPU temperature
@@ -76,8 +121,6 @@ class DisplayRenderer:
         """Get updates for the display"""
         self.draw.rectangle((204, 68, 248, 109), fill=255)
 
-        self.draw.text((204, 67), "apt", font=self.font14, fill=0)
-        self.draw.line([(226, 76), (248, 76)], fill=0, width=0)
         number_of_updates = SystemOperations().check_updates(self.is_root)
         if number_of_updates == 0:
             self.draw.text((214, 80), "\ue8e8", font=self.nfont24, fill=0)
@@ -92,9 +135,6 @@ class DisplayRenderer:
         """Check network status"""
         self.draw.rectangle((249, 79, 296, 109), fill=255)
 
-        self.draw.text((250, 67), "net", font=self.font14, fill=0)
-        self.draw.line([(249, 76), (249, 110)], fill=0, width=0)
-        self.draw.line([(272, 76), (297, 76)], fill=0, width=0)
         if NetworkOperations.check_internet_connection():
             self.draw.text((260, 80), "\ue2bf", font=self.nfont24, fill=0)
         else:
@@ -120,9 +160,6 @@ class DisplayRenderer:
         """Render memory stats"""
         self.draw.rectangle((0, 73, 97, 109), fill=255)
 
-        self.draw.text((1, 67), "mem", font=self.font14, fill=0)
-        self.draw.line([(34, 76), (97, 76)], fill=0, width=0)
-        self.draw.text((10, 80), "\ue322", font=self.nfont24, fill=0)  # Unicode icon for memory
         self.draw.text(
             (40, 80), f"{SystemOperations.get_mem()}%", font=self.font24, fill=0
         )  # Memory percentage
@@ -133,18 +170,9 @@ class DisplayRenderer:
         """Render NVME stats"""
         self.draw.rectangle((111, 0, 202, 68), fill=255)
 
-        # DISK
-        self.draw.text((100, 0), "nvme", font=self.font14, fill=0)
-        self.draw.line([(138, 10), (203, 10)], fill=0, width=0)
-        self.draw.text(
-            (108, 12), "\uf7a4", font=self.nfont24, fill=0
-        )  # Unicode icon for Hard Drive
         self.draw.text(
             (138, 12), f"{SystemOperations.get_nvme_usage()}%", font=self.font24, fill=0
         )  # CPU percentage
-        self.draw.text(
-            (108, 42), "\ue1ff", font=self.nfont24, fill=0
-        )  # Unicode icon for temperature
         self.draw.text(
             (138, 42), f"{SystemOperations.get_nvme_temp()}°C", font=self.font24, fill=0
         )  # Nvme temperature
@@ -155,9 +183,6 @@ class DisplayRenderer:
         """Render fan speed"""
         self.draw.rectangle((100, 70, 202, 109), fill=255)
 
-        self.draw.text((100, 67), "fan", font=self.font14, fill=0)
-        self.draw.line([(122, 76), (203, 76)], fill=0, width=0)
-        self.draw.text((108, 80), "\uf168", font=self.nfont24, fill=0)  # Unicode icon for fan
         self.draw.text(
             (138, 80), f"{SystemOperations.get_fan_speed()}", font=self.font24, fill=0
         )  # Fan speed
@@ -169,7 +194,6 @@ class DisplayRenderer:
         self.draw.rectangle((0, 111, 109, 128), fill=255)
 
         ip_address = NetworkOperations.get_ip_address()
-        self.draw.text((5, 110), "\ue80d", font=self.nfont14, fill=0)  # Unicode icon for network
 
         if ip_address:
             self.draw.text((20, 110), f"{ip_address}", font=self.font14, fill=0)  # Ip address
@@ -183,8 +207,6 @@ class DisplayRenderer:
         self.draw.rectangle((205, 111, 296, 128), fill=255)
 
         uptime = SystemOperations.get_uptime()
-
-        self.draw.text((205, 110), "\ue923", font=self.nfont14, fill=0)  # Unicode icon for uptime
         self.draw.text(
             (220, 110), f"{uptime[0]}d {uptime[1]}h {uptime[2]}m", font=self.font14, fill=0
         )  # uptime
@@ -196,16 +218,9 @@ class DisplayRenderer:
         self.draw.rectangle((204, 0, 296, 68), fill=255)
 
         network = TrafficMonitor().get_current_traffic()
-        self.draw.text((204, 0), "down", font=self.font14, fill=0)
-        self.draw.line([(242, 10), (261, 10)], fill=0, width=0)
         self.draw.text((263, 0), f"{network[1]}/s", font=self.font14, fill=0)
-        self.draw.text((208, 10), "\uf090", font=self.nfont24, fill=0)  # Unicode icon download
         self.draw.text((233, 14), f"{round(network[0], 2)}", font=self.font20, fill=0)  # download
-
-        self.draw.text((204, 33), "up", font=self.font14, fill=0)
-        self.draw.line([(222, 43), (261, 43)], fill=0, width=0)
         self.draw.text((263, 33), f"{network[3]}/s", font=self.font14, fill=0)
-        self.draw.text((208, 44), "\uf09b", font=self.nfont24, fill=0)  # Unicode icon for upload
         self.draw.text((233, 48), f"{round(network[2], 2)}", font=self.font20, fill=0)  # upload
 
         self.update_display_and_save_image()
